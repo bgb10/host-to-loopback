efd8a8440f78d2b65c9fe8149e254c513f2bbf0b
bgb10 <bgb10@naver.com>
2023-10-25 16:00:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Host to Loopback
+
+Maps hostname to loopback address by modifying /etc/hosts, which blocks outbound traffic.
+Written in Bash shell script, works on Linux/Unix based OS(ubuntu, OSX, etc).
+
+```shell
+# Usage: $0 [-a(add)/-d(delete)] <URL>
+
+./host_to_loopback.sh -a www.google.com
+./host_to_loopback.sh -d www.google.com
+```
---
+++ host_to_loopback.sh
@@ -0,0 +1,40 @@
+USAGE="Usage: $0 [-a/-d] <URL>"
+
+if [ $# -eq 0 ] 
+then 
+    echo $USAGE
+    exit 1
+fi
+
+while getopts a:d: opt
+do
+    case $opt in
+        a) 
+            url=$OPTARG
+            new_line="127.0.0.1\t$url"
+            
+            if grep -q "$new_line" /etc/hosts; then
+                echo "Line for $url already exists in /etc/hosts. No changes made."
+            else
+                echo -e "$new_line" | sudo tee -a /etc/hosts > /dev/null
+                echo "Line added for $url in /etc/hosts."
+            fi
+            ;;
+        d) 
+            url=$OPTARG
+            new_line="127.0.0.1\t$url"
+
+            if grep -q "$new_line" /etc/hosts; then
+                sudo grep -v "$new_line" /etc/hosts > /etc/hosts.tmp
+                sudo mv /etc/hosts.tmp /etc/hosts
+                echo "Line for $url deleted from /etc/hosts."
+            else
+                echo "No changes made for $url as it does not exist in /etc/hosts."
+            fi
+            ;;  
+        *) 
+            echo $USAGE
+            exit 1
+            ;;
+    esac
+done
